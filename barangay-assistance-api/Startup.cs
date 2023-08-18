955edf4bc36bc0e37c030c171d72c7490461c873
JM Gabriel <johnmark@sdsolutions.com.ph>
2023-08-18 19:32:11 +0800

    - remove routeprefix in swaggerUI

--- barangay-assistance-api/Startup.cs
+++ barangay-assistance-api/Startup.cs
@@ -91,7 +91,11 @@ namespace barangay_assistance_api
             }
 
             app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "barangay_assistance_api v1"));
+            app.UseSwaggerUI(c => 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "barangay_assistance_api v1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
